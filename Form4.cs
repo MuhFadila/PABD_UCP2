baf9be06ad1b7d79407e820aff51e22661964fae
Muhammad Fadila <muh.fadila18@gmail.com>
2023-06-15 20:36:45 +0700

    commit

---
+++ Form4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PABD_UCP2
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'prodiTIDataSet2.Nilai' table. You can move, or remove it, as needed.
+            this.nilaiTableAdapter.Fill(this.prodiTIDataSet2.Nilai);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            form1.Show();
+            this.Hide();
+        }
+    }
+}
